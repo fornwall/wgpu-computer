e250245a00d3f7c08c5af76adc460e42ea0f596d
Fredrik Fornwall <fredrik@fornwall.net>
2023-08-20 06:54:57 +0200

    Update tests

---
+++ run-browser-tests.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import json
+from os import listdir
+from os.path import isfile, join, abspath
+import webbrowser
+
+test_cases = []
+
+for f in listdir("tests"):
+    if f.endswith(".wgsl"):
+        with open(f'tests/{f}','r') as file:
+            source = file.read().strip();
+        expected_filename = f.replace('.wgsl', '.expected')
+        with open(f'tests/{expected_filename}','r') as file:
+            expected = file.read().strip();
+        test_cases.append({
+            "name": f,
+            "source": source,
+            "expected": expected
+        })
+
+test_cases_json = json.dumps(test_cases)
+
+with open(f'template.html','rt') as file:
+    template = file.read().strip();
+
+test_html = template.replace('__TESTCASES__', test_cases_json);
+
+with open(f'test.html','wt') as file:
+    file.write(test_html)
+
+test_html_path = abspath("test.html")
+print(test_html_path)
+webbrowser.open('file://' + test_html_path, new=True)
+
--- run-tests.sh
+++ run-tests.sh
@@ -6,8 +6,18 @@ any_error=false
 for input_filename in tests/*.wgsl; do
     expected_filename=$(echo "$input_filename" | sed "s/.wgsl/.expected/")
     expected_output=$(cat "$expected_filename")
+    nagabranch_filename=$(echo "$input_filename" | sed "s/.wgsl/.nagabranch/")
+    if [ -f "$nagabranch_filename" ]; then
+        while read -r naga_repo && read -r naga_branch; do
+            printf "[patch.\"https://github.com/gfx-rs/naga\"]\nnaga = { git = \"%s\", branch = \"%s\" }" "$naga_repo" "$naga_branch" >> Cargo.toml
+        done < "$nagabranch_filename"
+    fi
+
     printf "%s" "Checking $input_filename ..."
     actual_output=$(cargo run -q -- "$input_filename")
+
+    git checkout --quiet Cargo.toml
+
     if [ "$actual_output" = "$expected_output" ]; then
         echo " Ok!"
     else
