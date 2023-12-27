f06284d46a83f959ec83331da94b5c659881c204
Fredrik Fornwall <fredrik@fornwall.net>
2023-12-27 23:24:01 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,8 @@ pub async fn compute() -> Result<f32, String> {
     let (device, queue) = adapter
         .request_device(
             &wgpu::DeviceDescriptor {
-                features: wgpu::Features::empty(),
-                limits: wgpu::Limits::default(),
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::default(),
                 label: None,
             },
             None,
